0753314bd0e3c29417991be0d5ff105f57a45dcc
George Benjamin <msdcconnect@gmail.com>
2018-07-23 20:47:16 +0100

    Added NewQuoteActivity

--- app/src/main/java/com/kurobarabenjamingeorge/quoteme/NewQuoteActivity.java
+++ app/src/main/java/com/kurobarabenjamingeorge/quoteme/NewQuoteActivity.java
@@ -2,17 +2,12 @@ package com.kurobarabenjamingeorge.quoteme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class NewQuoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void loadNewQuoteActivity(View view) {
-
+        setContentView(R.layout.activity_new_quote);
     }
 }
